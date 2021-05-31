6c7c228dfc2a69c93102cfffa9c620fba233b414
hongcheol <tlsghdcjf41@gmail.com>
2021-05-31 22:34:13 +0900

    소셜로그인 정보 받아오는 부분 체크 완료. 컨트롤러에서 처리할 예정

--- src/main/java/com/example/fourart/controller/OAuth2Controller.java
+++ src/main/java/com/example/fourart/controller/OAuth2Controller.java
@@ -6,8 +6,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.User;
 import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,7 +32,9 @@ public class OAuth2Controller {
     }
 
     @GetMapping({"/loginSuccess","/success"})
-    public String loginSuccess(){
+    public String loginSuccess(@AuthenticationPrincipal OAuth2User oAuth2User){
+        log.info("Social Login Info");
+        log.info(oAuth2User.toString());
         return "signup";
     }
 
