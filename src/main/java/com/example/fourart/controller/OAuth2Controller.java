5493e008ef3417489938c95d812e94d133c49239
hongcheol <tlsghdcjf41@gmail.com>
2021-06-17 23:50:58 +0900

    instagram access token 받아오기 구현

--- src/main/java/com/example/fourart/controller/OAuth2Controller.java
+++ src/main/java/com/example/fourart/controller/OAuth2Controller.java
@@ -1,38 +1,48 @@
 package com.example.fourart.controller;
 
-import com.example.fourart.entity.InterestingSubject;
+import com.example.fourart.entity.InstaOAuth2Token;
 import com.example.fourart.entity.Member;
 import com.example.fourart.entity.Role;
 import com.example.fourart.service.InstaConnectService;
 import com.example.fourart.service.MemberService;
 import com.example.fourart.service.SocialLoginService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.User;
-import org.apache.commons.collections4.map.UnmodifiableMap;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.server.Session;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.*;
+import java.net.http.HttpClient;
 import java.util.ArrayList;
 import java.util.Collections.*;
+import javax.net.ssl.HttpsURLConnection;
 import javax.validation.Valid;
 import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.Collection;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -92,16 +102,43 @@ public class OAuth2Controller {
     }
 
     @GetMapping("/insta_conn_success")
-    public String instagramConnect(){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Member member = (Member) authentication.getPrincipal();
-        log.info("hello");
-        return "";
+    public String instagramConnect(@RequestParam String code) throws IOException {
+        log.info("123456789");
+        log.info(code);
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type","application/x-www-form-urlencoded; charset=UTF-8");
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+
+        params.add("client_id","213487120598037");
+        params.add("client_secret","988a59949faa7ef4d301c95d1f0cd9a7");
+        params.add("code",code);
+        params.add("grant_type","authorization_code");
+        params.add("redirect_uri","https://localhost:8443/insta_conn_success");
+
+        HttpEntity<MultiValueMap<String,String>> request = new HttpEntity<>(params,headers);
+
+        ResponseEntity response = restTemplate.exchange(
+                "https://api.instagram.com/oauth/access_token",
+                HttpMethod.POST,
+                request,
+                String.class
+        );
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        InstaOAuth2Token oToken = null;
+
+        try {
+            oToken = objectMapper.readValue(response.getBody().toString(), InstaOAuth2Token.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        log.info(oToken.getAccess_token());
+        return "home";
     }
-
-    @PostMapping("/insta_conn_success")
-
-
     @GetMapping("/loginFail")
     public String loginFail(){
         return "Landing_page";
