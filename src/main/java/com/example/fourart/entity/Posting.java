aefaf36dccb6b8521da77f1dfaa227fc09c50ff2
hongcheol <tlsghdcjf41@gmail.com>
2021-06-26 21:22:51 +0900

    해시태그 리스트 만들기, 포스팅 카테고리 구분 포스팅 엔티티 설계(게시글 해시태그는 요구사항 명확히 정해지면 반영 예정)

--- src/main/java/com/example/fourart/entity/Posting.java
+++ src/main/java/com/example/fourart/entity/Posting.java
@@ -1,12 +1,24 @@
 package com.example.fourart.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 @Entity
+@Getter @Setter
+@Table(name = "posting")
 public class Posting {
     @Id
     @GeneratedValue
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member author;
+
+    @Column(name = "category")
+    private PostingCategory postingCategory;
+
+
 }
