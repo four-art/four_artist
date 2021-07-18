99f21ab499ff9e02da1b2c32ad48955a362cf028
hongcheol <tlsghdcjf41@gmail.com>
2021-07-18 19:27:40 +0900

    멤버에 해시태그 추가 기능용 레포지토리(interface로 전환)와 서비스 수정

--- src/main/java/com/example/fourart/repository/MemberRepository.java
+++ src/main/java/com/example/fourart/repository/MemberRepository.java
@@ -2,37 +2,13 @@ package com.example.fourart.repository;
 
 import com.example.fourart.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public class MemberRepository {
-    @PersistenceContext
-    private EntityManager em;
-
-    public void save(Member member){
-        em.persist(member);
-    }
-
-    public Member findOne(Long id){
-        return em.find(Member.class, id);
-    }
-
-    public List<Member> findAll(){
-        return em.createQuery("select m from Member m",Member.class).getResultList();
-    }
-
-    public Member findByEmail(String email){
-        return em.find(Member.class,email);
-    }
-    public List<Member> findAllByEmail(String email){
-        return em.createQuery("select m from Member m where m.email = :email",Member.class)
-                .setParameter("email",email)
-                .getResultList();
-    }
-
+public interface MemberRepository extends JpaRepository<Member,Long>{
+    List<Member> findAllByEmail(@Param("email")String email);
+    Member findByEmail(@Param("email")String email);
 }
